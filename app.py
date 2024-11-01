d5e93d1004a88b46834ff6d5292b7e7b6d17561c
Md. Imran Hossain <65802128+Imran012x@users.noreply.github.com>
2024-11-01 06:58:53 +0600

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def get_notices():
     return jsonify(notices_data)
 
 if __name__ == '__main__':
-    #port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
     app.run(host='0.0.0.0', port=port,debug=True)
 
 
