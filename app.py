702b0d207468ff92fc78f0948ae3115dbcb06cb6
Md. Imran Hossain <65802128+Imran012x@users.noreply.github.com>
2024-11-01 06:53:22 +0600

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 app = Flask(__name__)
 
 # Define the API route for scraping
-@app.route('/)
+@app.route('/')
 def index():
    return "Notice Board"# Define the API route for scraping
 @app.route('/api/notices', methods=['GET'])
@@ -15,6 +15,7 @@ def get_notices():
     return jsonify(notices_data)
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
     app.run(host='0.0.0.0', port=port,debug=True)
 
 
