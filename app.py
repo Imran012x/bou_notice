bf768a7f29855f5e8c471a9801cd3a44983be084
Md. Imran Hossain <65802128+Imran012x@users.noreply.github.com>
2024-11-01 06:38:47 +0600

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,6 @@ def get_notices():
     return jsonify(notices_data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)),debug=True)
+    app.run(host='0.0.0.0', port=port,debug=True)
 
 
